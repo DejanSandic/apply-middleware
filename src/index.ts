6177f132628a3fb0c3e82d604c969def6919c7db
Dejan Sandic <dejan.s@aleacontrol.com>
2019-11-14 10:38:00 +0100

    Fix linting issues

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,38 @@ export type Next = (...newArgs: any[]) => void;
 
 export type Middleware = (next: Next, ...args: any[]) => void;
 
-export default function applyMiddleware(target: Function, ...middlewares: Middleware[]) {
-	// Verify all arguments passed to the the applyMiddleware function are functions
-	for (const arg of arguments) {
-		if (typeof arg !== 'function') {
-			throw new Error('All arguments passed to the applyMiddleware function need to be functions.');
-		}
-	}
+export default function applyMiddleware (
+   target: Function,
+   ...middlewares: Middleware[]
+) {
+   // Verify all arguments passed to the the applyMiddleware function are functions
+   for (const arg of arguments) {
+      if (typeof arg !== 'function') {
+         throw new Error(
+            'All arguments passed to the applyMiddleware function need to be functions.'
+         );
+      }
+   }
 
-	// Function which recursively iterates over the middlewares
-	function apply(middleware: Middleware, rest: Middleware[], args: any[]) {
-		function next(...newArgs: any[]) {
-			// If next() was called without arguments
-			if (!newArgs.length) newArgs = args;
+   // Function which recursively iterates over the middlewares
+   function apply (middleware: Middleware, rest: Middleware[], args: any[]) {
+      function next (...newArgs: any[]) {
+         // If next() was called without arguments
+         if (!newArgs.length) newArgs = args;
 
-			const [ nextMiddleware, ...nextRest ] = rest;
-			apply(nextMiddleware, nextRest, newArgs);
-		}
+         const [nextMiddleware, ...nextRest] = rest;
+         apply(nextMiddleware, nextRest, newArgs);
+      }
 
-		if (middleware) middleware(next, ...args);
-		else target(...args);
-	}
+      if (middleware) middleware(next, ...args);
+      else target(...args);
+   }
 
-	// Function which starts iteration over middlewares
-	function execute(...args: any[]) {
-		const [ middleware, ...rest ] = middlewares;
-		apply(middleware, rest, args);
-	}
+   // Function which starts iteration over middlewares
+   function execute (...args: any[]) {
+      const [middleware, ...rest] = middlewares;
+      apply(middleware, rest, args);
+   }
 
-	return execute;
+   return execute;
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,15 +1,15 @@
 import applyMiddleware, { Next } from '../src/';
 
 const addOne = jest.fn((next: Next, num: number) => {
-	next(num + 1);
+   next(num + 1);
 });
 
 const doNothing = jest.fn((next: Next) => {
-	next();
+   next();
 });
 
 const addDecimal = jest.fn((next: Next, num: number) => {
-	next(num + 0.1);
+   next(num + 0.1);
 });
 
 const target = jest.fn();
@@ -17,20 +17,25 @@ const target = jest.fn();
 const next = expect.anything();
 
 test('applyMiddleware should invoke the target function with data modified by the middleware functions.', () => {
-	const wrapped = applyMiddleware(target, addOne, doNothing, addDecimal);
-	wrapped(11);
-	expect(addOne).toBeCalledWith(next, 11);
-	expect(doNothing).toBeCalledWith(next, 12);
-	expect(addDecimal).toBeCalledWith(next, 12);
-	expect(target).toBeCalledWith(12.1);
+   const wrapped = applyMiddleware(target, addOne, doNothing, addDecimal);
+   wrapped(11);
+   expect(addOne).toBeCalledWith(next, 11);
+   expect(doNothing).toBeCalledWith(next, 12);
+   expect(addDecimal).toBeCalledWith(next, 12);
+   expect(target).toBeCalledWith(12.1);
 });
 
 test('applyMiddleware should invoke the target function with data modified by the middleware functions.', () => {
-	const num: any = 11;
+   const num: any = 11;
 
-	const fn1 = () => applyMiddleware(num, addOne);
-	expect(fn1).toThrowError('All arguments passed to the applyMiddleware function need to be functions.');
+   const fn1 = () => applyMiddleware(num, addOne);
+   expect(fn1).toThrowError(
+      'All arguments passed to the applyMiddleware function need to be functions.'
+   );
 
-	const fn2 = () => applyMiddleware(target, addOne, doNothing, num, addDecimal);
-	expect(fn2).toThrowError('All arguments passed to the applyMiddleware function need to be functions.');
+   const fn2 = () =>
+      applyMiddleware(target, addOne, doNothing, num, addDecimal);
+   expect(fn2).toThrowError(
+      'All arguments passed to the applyMiddleware function need to be functions.'
+   );
 });
